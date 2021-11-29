f17b57b1cc34583421dfa1588ea4cba1acfedffd
teknixco <vic@teknixco.net>
2021-11-29 08:56:43 +0000

    overloaded methods

--- objects/Victor.java
+++ objects/Victor.java
@@ -1,14 +1,32 @@
 class Person {
+	private String name;
+	private int age;
+	private String type;
+	public static String description = "This is a person class, describing a person with name, age, and type = human";
+
 	public Person() {
-		System.out.println("Constructor running!");
+		type = "human";
+		System.out.println(description);
+		System.out.println("Constructor running! "+ type);
 	}
 	
-	public Person(String name) {
+	public Person(String name) { 
+		this(); // Call default constructor
 		System.out.println("Second constructor running!");
 	}
 
 	public Person(String name, int age) {
-		System.out.println("Third constructor runnign!");
+		this(); // Call default constructor
+		System.out.println("Third constructor running!");
+	}
+
+	public void speak() {
+		System.out.println("Yo");
+	}
+	
+	public void speak(String name) {  //overloaded methods have the same name
+		this.name = name;
+		System.out.println("My name is "+name);
 	}
 }
 
@@ -16,7 +34,9 @@ public class Victor {
 	public static void main(String[] args) {
 		Person vic1 = new Person();
 		Person vic2 = new Person("Vic");
-		Person vic3 = new Person ("Vic", 34);
+		Person vic3 = new Person ("Vic", 77);
+		vic3.speak();
+		vic3.speak("Victor");
 	}
 }
 
