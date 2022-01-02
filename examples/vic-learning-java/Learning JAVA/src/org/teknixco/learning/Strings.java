4bdd1a1a552cec5d4bde827a39c17aa7b3f56eb8
Victor Ekpo <victor.ekpo@gmail.com>
2022-01-02 17:30:06 -0600

    learning Java

--- examples/vic-learning-java/Learning JAVA/src/org/teknixco/learning/Strings.java
+++ examples/vic-learning-java/Learning JAVA/src/org/teknixco/learning/Strings.java
@@ -4,6 +4,9 @@ public class Strings {
 	public static void main(String[] args) {
 		int myInt = 7;
 		String text = "Hello baby";
-		System.out.println(text);
+		String blank = " ";
+		String name = "IA";
+		String greeting = text + blank + name;
+		System.out.println(greeting);
 	}
 }
