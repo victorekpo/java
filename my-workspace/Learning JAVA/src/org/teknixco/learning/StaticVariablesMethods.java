4d727f744e213997b0948ee2cd33d86c4818785d
Victor Ekpo <victor.ekpo@gmail.com>
2022-01-03 14:01:18 -0600

    learning Java

--- my-workspace/Learning JAVA/src/org/teknixco/learning/StaticVariablesMethods.java
+++ my-workspace/Learning JAVA/src/org/teknixco/learning/StaticVariablesMethods.java
@@ -3,12 +3,18 @@ package org.teknixco.learning;
 // Static member variables belong to the class, and copies are not made. 
 // Static methods are accessed by the class name. Static methods cannot refer to non-static (instance) variables/methods.
 // But instance methods (non-static methods) can access static data. 
+// A good use of static is to set class constants such as Math.PI or to count the number of objects.
 
 class Thing {
 	public String name;
 	public static String description;
 	public static final int LUCKY_NUMBER = 7; // class constant
-
+	public static int count = 0;
+	
+	public Thing() {
+		count++; // constructor increments count every time an instance is created.
+	}
+	
 	public void getName() {
 		System.out.println(description + ": " + name);
 	}
@@ -27,6 +33,7 @@ public class StaticVariablesMethods {
 		Thing.description = "I am a thing";
 		Thing.showInfo();
 		System.out.println(Thing.description);
+		System.out.println("Current count of Thing objects are: " + Thing.count);
 
 		thing1.name = "Bob";
 		thing2.name = "Sue";
