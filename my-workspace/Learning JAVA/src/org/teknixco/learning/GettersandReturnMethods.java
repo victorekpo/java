b8bc5eb07c5aded6e645fa5881e04810fc02c12a
Victor Ekpo <victor.ekpo@gmail.com>
2022-01-03 13:08:37 -0600

    learning Java

--- my-workspace/Learning JAVA/src/org/teknixco/learning/GettersandReturnMethods.java
+++ my-workspace/Learning JAVA/src/org/teknixco/learning/GettersandReturnMethods.java
@@ -5,25 +5,27 @@ class PersonGetters {
 	private int age;
 
 	void speak() {
-		System.out.println("Hello, my name is " +name+ ", I am " + age + " years old.");
+		System.out.println("Hello, my name is " + name + ", I am " + age + " years old.");
 	}
-	
+
 	int calculateYearsToRetirment() {
 		int yearsLeft = 75 - age;
 		// System.out.println(yearsLeft);
 		return yearsLeft;
 	}
-	
+
 	int getAge() {
 		return age;
 	}
-	
-	int setAge(int age) {
-		return this.age = age;
+
+	void setAge(int age) {
+		this.age = age;
 	}
-	
-	// Encapsulation is important because you will need to hide certain program variables for security.
+
+	// Encapsulation is important because you will need to hide certain program
+	// variables for security.
 	// Getter methods allow you to get values that are hidden.
+	// Setter methods allow you to set values.
 
 }
 
@@ -33,7 +35,7 @@ public class GettersandReturnMethods {
 		person1.name = "Victor E.";
 		person1.setAge(34);
 		PersonGetters person2 = new PersonGetters(); // the keyword "new" sets the type "Person" for person2 to
-																// a new Person object.
+														// a new Person object.
 		person2.name = "Someone Special";
 		person2.setAge(25);
 		person1.speak();
