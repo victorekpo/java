38ab9bb0a297c1ba48aaaf5e1cf87099cb94934f
Victor Ekpo <victor.ekpo@gmail.com>
2022-01-02 18:09:45 -0600

    learning Java

--- examples/vic-learning-java/Learning JAVA/src/org/teknixco/learning/UserInput.java
+++ examples/vic-learning-java/Learning JAVA/src/org/teknixco/learning/UserInput.java
@@ -1,8 +1,14 @@
 package org.teknixco.learning;
+import java.util.Scanner;
 
 public class UserInput {
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in); // instantiates input variable
 		
-		Scanner 
+		System.out.println("Enter a line of text: "); //prompts user to input text 
+		
+		String line = input.nextLine(); // converts the input to a string
+		
+		System.out.println("You entered: " + line);
 	}
 }
