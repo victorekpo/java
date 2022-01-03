ba16c9e543999672ad54cee7b31d9404289def4a
Victor Ekpo <victor.ekpo@gmail.com>
2022-01-02 21:16:01 -0600

    learning Java

--- my-workspace/Learning JAVA/src/org/teknixco/learning/ArraysofStrings.java
+++ my-workspace/Learning JAVA/src/org/teknixco/learning/ArraysofStrings.java
@@ -2,6 +2,18 @@ package org.teknixco.learning;
 
 public class ArraysofStrings {
 	public static void main(String[] args) {
+		String[] words = new String[3];
+		
+		words[0] = "Hello ";
+		words[1] = "baby ";
+		words[2] = "mmi";
+		System.out.println(words[0] + words[1] + words[2]);
+		
+		String[] greeting = {"Hello", "my", "darling"};
+		
+		for(String word: greeting) { // easy way to use a for loop for each element, for greet in greeting. 
+			System.out.println(word);
+		}
 		
 	}
 }
