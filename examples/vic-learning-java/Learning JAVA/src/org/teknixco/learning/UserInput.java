afe764ff3e3179ce50e30e0ed363b1c5888353e1
Victor Ekpo <victor.ekpo@gmail.com>
2022-01-02 20:20:59 -0600

    learning Java

--- examples/vic-learning-java/Learning JAVA/src/org/teknixco/learning/UserInput.java
+++ examples/vic-learning-java/Learning JAVA/src/org/teknixco/learning/UserInput.java
@@ -7,8 +7,16 @@ public class UserInput {
 		
 		System.out.println("Enter a line of text: "); //prompts user to input text 
 		
-		String line = input.nextLine(); // converts the input to a string
+		String line = input.nextLine(); // converts the input to a string, another method is nextInt() for integers, will crash if not integer, nextDouble() is another method. Keep in mind in different parts of the world that doubles are typed differently.
 		
 		System.out.println("You entered: " + line);
+		
+		Scanner input2 = new Scanner(System.in); // instantiates input variable
+		
+		System.out.println("Enter a double: "); //prompts user to input text
+		
+		double value = input2.nextDouble(); // throws error if not a number
+		
+		System.out.println("You entered: " + value);
 	}
 }
