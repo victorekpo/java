aead8ac37087ad98446002dc32dd58cc72e3f567
Victor Ekpo <victor.ekpo@gmail.com>
2022-01-02 18:03:16 -0600

    learning Java

--- examples/vic-learning-java/Learning JAVA/src/org/teknixco/learning/IfStatements.java
+++ examples/vic-learning-java/Learning JAVA/src/org/teknixco/learning/IfStatements.java
@@ -19,7 +19,10 @@ public class IfStatements {
 		if(myInt < 30) {
 			System.out.println(trueStatement);
 		}
-		else { // only use else if you have binary conditions, that is only 2 possibilities, otherwise use elseif, switch statements, objects, etc.
+		else if(myInt < 50) { // two conditions to check using else if
+			System.out.println(trueStatement);
+		}
+		else { // only use else, if you have binary conditions, that is only 2 possibilities, otherwise use else if, switch statements, objects, etc. Else can also be considered the final check (or everything else).
 			System.out.println(falseStatement);
 		}
 	}
