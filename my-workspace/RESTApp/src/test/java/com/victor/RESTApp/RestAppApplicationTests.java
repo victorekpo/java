445e26cc2f6eaaeebb0fd37941da9a5683f27328
Victor Ekpo <victor.ekpo@gmail.com>
2022-02-01 20:36:27 -0600

    add RESTApp via Spring Boot framework

---
+++ my-workspace/RESTApp/src/test/java/com/victor/RESTApp/RestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.RESTApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
