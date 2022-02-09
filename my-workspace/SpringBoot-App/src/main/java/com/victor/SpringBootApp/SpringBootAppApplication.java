0910e76169f6d5f53d7526b34a8a85aecd31caa1
Vic <victor.ekpo@gmail.com>
2022-02-08 23:07:58 -0600

    springboot app v1

---
+++ my-workspace/SpringBoot-App/src/main/java/com/victor/SpringBootApp/SpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.victor.SpringBootApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppApplication.class, args);
+	}
+
+}
