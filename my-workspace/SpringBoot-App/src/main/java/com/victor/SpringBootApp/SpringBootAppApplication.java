35ae5ba3b327289193c04950296e0bf255c8489c
Vic <victor.ekpo@gmail.com>
2022-02-10 18:42:53 -0600

    updated

--- my-workspace/SpringBoot-App/src/main/java/com/victor/SpringBootApp/SpringBootAppApplication.java
+++ my-workspace/SpringBoot-App/src/main/java/com/victor/SpringBootApp/SpringBootAppApplication.java
@@ -1,13 +1,11 @@
 package com.victor.SpringBootApp;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@EnableAutoConfiguration
+@SpringBootApplication(scanBasePackages = "com.victor")
 @EntityScan(basePackages = {"com.victor"})
 @ComponentScan(basePackages = "com.victor")
 public class SpringBootAppApplication {
