6cc8aa19a908ce24c6b19b3d3bb760af4cb56498
Vic <victor.ekpo@gmail.com>
2022-02-10 18:46:59 -0600

    added fix

--- my-workspace/SpringBoot-App/src/main/java/com/victor/SpringBootApp/SpringBootAppApplication.java
+++ my-workspace/SpringBoot-App/src/main/java/com/victor/SpringBootApp/SpringBootAppApplication.java
@@ -3,11 +3,9 @@ package com.victor.SpringBootApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(scanBasePackages = "com.victor")
 @EntityScan(basePackages = {"com.victor"})
-@ComponentScan(basePackages = "com.victor")
 public class SpringBootAppApplication {
 
 	public static void main(String[] args) {
