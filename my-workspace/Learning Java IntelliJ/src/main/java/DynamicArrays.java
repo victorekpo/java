d5ea2ac27b052c6a156e9a1280d1c4712ead6d66
Vic <victor.ekpo@gmail.com>
2022-02-18 17:43:08 -0600

    updated

---
+++ my-workspace/Learning Java IntelliJ/src/main/java/DynamicArrays.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class DynamicArrays {
+    public static void main(String[] args) {
+        System.out.println("Test");
+        List<String> newList = new ArrayList();
+        newList.add("Testing");
+        newList.add("123");
+        newList.add("another one");
+        System.out.println(newList);
+    }
+}
