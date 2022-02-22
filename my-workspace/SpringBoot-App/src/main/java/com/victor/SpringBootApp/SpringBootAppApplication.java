76e41595887275e5902e6d3410b471aa67ce0ded
Vic <victor.ekpo@gmail.com>
2022-02-21 21:47:21 -0600

    updated

--- my-workspace/SpringBoot-App/src/main/java/com/victor/SpringBootApp/SpringBootAppApplication.java
+++ my-workspace/SpringBoot-App/src/main/java/com/victor/SpringBootApp/SpringBootAppApplication.java
@@ -3,14 +3,7 @@ package com.victor.SpringBootApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 //Normally the main class will be in the root of the project to pick up the other classes, if not explicitly pass in the basePackage settings.
 @SpringBootApplication(scanBasePackages = "com.victor")
@@ -21,15 +14,6 @@ public class SpringBootAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootAppApplication.class, args);
 	}
-
-		@Bean
-		public Docket api() {
-			return new Docket(DocumentationType.SWAGGER_2)
-					.select()
-					.apis(RequestHandlerSelectors.any())
-					.paths(PathSelectors.any())
-					.build();
-		}
 
 
 
