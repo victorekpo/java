0e82bc238637f1158ed086c6c1504110f6e8a9ef
Vic <victor.ekpo@gmail.com>
2024-12-03 19:31:58 -0600

    upgrade to spring boot 3.4.0

--- spring/app1/src/test/java/com/example/demo/ApplicationTests.java
+++ spring/app1/src/test/java/com/example/demo/ApplicationTests.java
@@ -1,15 +1,15 @@
-package com.example.demo;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
-class ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.example.demo;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.context.annotation.Import;
+//
+//@Import(TestcontainersConfiguration.class)
+//@SpringBootTest
+//class ApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
--- spring/app1/src/test/java/com/example/demo/TestDemoApplication.java
+++ spring/app1/src/test/java/com/example/demo/TestDemoApplication.java
@@ -1,11 +1,11 @@
-package com.example.demo;
-
-import org.springframework.boot.SpringApplication;
-
-public class TestDemoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.from(Application::main).with(TestcontainersConfiguration.class).run(args);
-	}
-
-}
+//package com.example.demo;
+//
+//import org.springframework.boot.SpringApplication;
+//
+//public class TestDemoApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.from(Application::main).with(TestcontainersConfiguration.class).run(args);
+//	}
+//
+//}
