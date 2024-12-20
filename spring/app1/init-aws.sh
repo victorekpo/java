e7b5afd12f49db367ff62530d8a47e6993cd35b5
Victor Ekpo <algorithm.v@Mac.lan>
2024-12-19 18:27:38 -0600

    localstack init script

--- spring/app1/init-aws.sh
+++ spring/app1/init-aws.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Set the AWS region
+export AWS_DEFAULT_REGION=us-east-1
+export AWS_ACCESS_KEY_ID=test
+export AWS_SECRET_ACCESS_KEY=test
+
 awslocal sqs create-queue --queue-name my-queue
 awslocal sqs create-queue --queue-name my-queue-dlq
 redrivePolicy="'{\"maxReceiveCount\":5,\"deadLetterTargetArn\":\"arn:aws:sqs:us-east-1:000000000000:my-queue-dlq\"}'"
