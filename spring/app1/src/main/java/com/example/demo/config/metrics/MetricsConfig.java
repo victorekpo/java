87438f62c686ca71a8d94e10ab70bb48b600cc39
algorithm.v <victor.ekpo@gmail.com>
2025-01-17 20:19:31 -0600

    micrometer influxdb

---
+++ spring/app1/src/main/java/com/example/demo/config/metrics/MetricsConfig.java
@@ -0,0 +1,55 @@
+//package com.example.demo.config.metrics;
+//
+//import io.micrometer.core.instrument.MeterRegistry;
+//import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class MetricsConfig {
+//
+//    @Bean
+//    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+//        return registry -> registry.config().commonTags("region", "us-east-1");
+//    }
+/// /
+/// /    @Bean
+/// /    public InfluxMeterRegistry influxMeterRegistry(InfluxConfig influxConfig) {
+/// /        return new InfluxMeterRegistry(influxConfig, Clock.SYSTEM);
+/// /    }
+/// /
+/// /    @Bean
+/// /    public InfluxConfig influxConfig() {
+/// /        return new InfluxConfig() {
+/// /            @Override
+/// /            public String get(String key) {
+/// /                return null; // accept the default values for all properties
+/// /            }
+/// /
+/// /            @Override
+/// /            public String uri() {
+/// /                return "http://localhost:8086";
+/// /            }
+/// /
+/// /            @Override
+/// /            public String db() {
+/// /                return "metricsdb";
+/// /            }
+/// /
+/// /            @Override
+/// /            public String userName() {
+/// /                return "vic";
+/// /            }
+/// /
+/// /            @Override
+/// /            public String password() {
+/// /                return "vic12345";
+/// /            }
+/// /
+/// /            @Override
+/// /            public String token() {
+/// /                return "secret-token";
+/// /            }
+/// /        };
+/// /    }
+//}
